e4f045e7976a0f9ca8439e87efebe4b458e6712b
yhb1226 <3537311140@qq.com>
2026-07-03 23:15:49 +0800

    读写锁代码修改

---
+++ Thread_test/rwlock_test_write_lock.c
@@ -0,0 +1,67 @@
+//静态锁用在完自己销毁，动态锁要手动销毁
+//此代码已经加上写的锁
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+
+// 静态初始化方式（可选，和动态init二选一）
+// static pthread_rwlock_t rwlock = PTHREAD_RWLOCK_INITIALIZER;
+pthread_rwlock_t rwlock;
+int shared_data = 0;
+
+// 读线程：加读锁读取共享数据（读锁可被多个线程同时持有）
+void * read_writer(void * arg){
+    pthread_rwlock_rdlock(&rwlock);
+    printf("当前是%s, shared_data为%d\n", (char *)arg, shared_data);
+    pthread_rwlock_unlock(&rwlock);
+    return NULL;
+}
+
+// 写线程：加写锁修改共享数据（写锁独占，同一时间仅一个写线程）
+void * lock_writer(void * arg){
+    pthread_rwlock_wrlock(&rwlock);
+    int tmp = shared_data + 1;
+    sleep(1);
+    shared_data = tmp;
+    printf("当前是%s, shared_data为%d\n", (char *)arg, shared_data);
+    pthread_rwlock_unlock(&rwlock);
+    return NULL;
+}
+
+int main(int argc, char const *argv[])
+{
+    // 动态初始化读写锁
+    pthread_rwlock_init(&rwlock, NULL);
+
+    pthread_t writer1, writer2;
+    pthread_t reader1, reader2, reader3, reader4, reader5, reader6;
+
+    // 创建2个写线程
+    pthread_create(&writer1, NULL, lock_writer, "writer1");
+    pthread_create(&writer2, NULL, lock_writer, "writer2");
+
+    // 休眠等待写线程执行
+    sleep(3);
+
+    // 创建6个读线程
+    pthread_create(&reader1, NULL, read_writer, "reader1");
+    pthread_create(&reader2, NULL, read_writer, "reader2");
+    pthread_create(&reader3, NULL, read_writer, "reader3");
+    pthread_create(&reader4, NULL, read_writer, "reader4");
+    pthread_create(&reader5, NULL, read_writer, "reader5");
+    pthread_create(&reader6, NULL, read_writer, "reader6");
+
+    // 等待所有子线程执行完毕
+    pthread_join(writer1, NULL);
+    pthread_join(writer2, NULL);
+    pthread_join(reader1, NULL);
+    pthread_join(reader2, NULL);
+    pthread_join(reader3, NULL);
+    pthread_join(reader4, NULL);
+    pthread_join(reader5, NULL);
+    pthread_join(reader6, NULL);
+
+    // 销毁读写锁
+    pthread_rwlock_destroy(&rwlock);
+    return 0;
+}
