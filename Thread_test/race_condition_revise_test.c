a2393ebb38656b2b90dd8f029dd5e295a68705ae
yhb1226 <3537311140@qq.com>
2026-07-03 16:00:43 +0800

    更新

---
+++ Thread_test/race_condition_revise_test.c
@@ -0,0 +1,43 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define THREAD_COUNT 20000
+
+// 初始化互斥锁
+static pthread_mutex_t counter_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+// 需要创建的多个线程
+void * add_thread(void * arg){
+    // 转化传入的参数
+    int *p = (int *)arg;
+    // 在累加之前 获取锁 保证同一时间只有一个线程对其累加
+    pthread_mutex_lock(&counter_mutex);
+    (*p)++;
+    // 累加完之后 释放锁
+    pthread_mutex_unlock(&counter_mutex);
+    return (void *)0;
+}
+
+int main(int argc, char const *argv[])
+{
+    pthread_t pid[THREAD_COUNT];
+    int num = 0;
+    for (size_t i = 0; i < THREAD_COUNT; i++)
+    {
+        // 创建的线程功能是给传入的参数累加1
+        pthread_create(pid + i,NULL,add_thread,&num);
+    }
+
+    // 等待所有的线程执行完成
+    for (size_t i = 0; i < THREAD_COUNT; i++)
+    {
+        pthread_join(pid[i],NULL);
+    }
+
+    // 打印最终的累加结果
+    printf("累加的结果是%d\n",num);
+
+    return 0;
+}
